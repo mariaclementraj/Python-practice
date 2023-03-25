d33e6f583956648bbab27fc9dc9b9c0ca457f9bc
mariaclementraj <128679902+mariaclementraj@users.noreply.github.com>
2023-03-25 11:23:45 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,15 @@
+title="STUDENT INFORMATION"
+name="CLEMENT"
+age=29
+email="mariaclementraj@gamil.com"
+address="ratna nagar, puducherry"
+mobile_no=9976816914
+print("* "*46)
+print("\t\t\t\t",title)
+print("* "*46)
+print("\t\t\tNAME\t\t:\t",name)
+print("\t\t\tAGE\t\t:\t",age)
+print("\t\t\tEMAIL\t\t:\t",email)
+print("\t\t\tADDRESS\t\t:\t",address)
+print("\t\t\tMOBILE NO\t:\t",mobile_no)
+print("* "*46)
