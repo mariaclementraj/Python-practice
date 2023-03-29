b9295ce58ca52335e15e0c065d3b92edd995bc9a
mariaclementraj <128679902+mariaclementraj@users.noreply.github.com>
2023-03-29 11:40:28 +0530

    Add files via upload

---
+++ arithmaticoperator.py
@@ -0,0 +1,32 @@
+#simple interest
+print("------------------------------------")
+print("Calculate simple interest")
+print("------------------------------------")
+p=int(input("Enter the Principle value "))
+n=int(input("Enter the number of year "))
+r=int(input("Enter the rate "))
+sim_inter=p*n*r/100
+print("Simple interest is = ",sim_inter)
+#Area of square
+print("------------------------------------")
+print("Calculate Area of square")
+print("------------------------------------")
+area=int(input("Enter the area value "))
+square=area*area
+print("Area of square is = ",square)
+#Area of Rectangle
+print("------------------------------------")
+print("Calculate Area of Rectangle")
+print("------------------------------------")
+l=int(input("Enter the length value "))
+w=int(input("Enter the width value "))
+rectangle=l*w
+print("Area of Rectangle is = ",rectangle)
+#Area of Triangle
+print("------------------------------------")
+print("Calculate Area of Triangle")
+print("------------------------------------")
+b=int(input("Enter the breadth value "))
+h=int(input("Enter the height value "))
+triangle=1/2*b*h
+print("Area of Triangle is = ",triangle)
